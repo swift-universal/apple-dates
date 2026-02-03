ae26b1a71f17f0a6b96f08f266f72d517a3f5b13
Cristian A Monterroza <1240948+rismay@users.noreply.github.com>
2026-02-03 04:43:49 -0800

    data: add android os release dates

--- sources/core-android-dates/OSReleaseDates.swift
+++ sources/core-android-dates/OSReleaseDates.swift
@@ -5,6 +5,47 @@ public typealias OSRelease = CorePlatformDates.OSRelease
 public struct OSReleaseDates: Sendable {
   // Dates use YYYY-MM-DD (public availability). Announcement dates are optional.
   public static let android: [OSRelease] = [
-    // TODO: backfill Android release dates with sources.
+    .init(
+      version: "10",
+      releaseDate: "2019-09-03",
+      sourceURLs: [
+        "https://android-developers.googleblog.com/2019/09/welcoming-android-10.html"
+      ]
+    ),
+    .init(
+      version: "11",
+      releaseDate: "2020-09-08",
+      sourceURLs: [
+        "https://android-developers.googleblog.com/2020/09/android-11-is-here.html"
+      ]
+    ),
+    .init(
+      version: "12",
+      releaseDate: "2021-10-04",
+      sourceURLs: [
+        "https://android-developers.googleblog.com/2021/10/android-12-is-live-in-aosp.html"
+      ]
+    ),
+    .init(
+      version: "13",
+      releaseDate: "2022-08-15",
+      sourceURLs: [
+        "https://android-developers.googleblog.com/2022/08/android-13-is-in-aosp.html"
+      ]
+    ),
+    .init(
+      version: "14",
+      releaseDate: "2023-10-04",
+      sourceURLs: [
+        "https://android-developers.googleblog.com/2023/10/android-14-is-released.html"
+      ]
+    ),
+    .init(
+      version: "15",
+      releaseDate: "2024-09-03",
+      sourceURLs: [
+        "https://android-developers.googleblog.com/2024/09/android-15-is-released-to-aosp.html"
+      ]
+    ),
   ]
 }
